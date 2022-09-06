f45cf82233d9ee9243c27a0b1aa40336d2b8d91b
joseviniciusb <josevbluz@outlook.com>
2022-09-06 12:46:07 -0300

    ft: add teams json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 // src/app.js
-const express = require('express');
+const express = require("express");
 
 const app = express();
+
+const teams = [
+  {
+    id: 1,
+    name: "São Paulo Futebol Clube",
+    initials: "SPF",
+  },
+  {
+    id: 2,
+    name: "Clube Atlético Mineiro",
+    initials: "CAM",
+  },
+];
+
+app.get("/", (req, res) => res.status(200).json({ message: 'olha ele ae ó' }));
+app.get("/teams", (req, res) => res.status(200).json({ message: teams }));
 
 module.exports = app;
