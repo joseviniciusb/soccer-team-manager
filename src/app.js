0bbc63bc6afa3334e67972c6fc5cb138145eae82
joseviniciusb <josevbluz@outlook.com>
2022-09-06 14:13:48 -0300

    ft: CRUD methods completed

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const teams = [
   {
@@ -16,7 +17,45 @@ const teams = [
   },
 ];
 
-app.get("/", (req, res) => res.status(200).json({ message: 'olha ele ae ó' }));
+
+// READ
+app.get("/", (req, res) => res.status(200).json({ message: "olha ele ae ó" }));
 app.get("/teams", (req, res) => res.status(200).json({ message: teams }));
+
+// Create
+app.post("/teams", (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+// Edit
+app.put("/teams/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+  let updatedTeam;
+
+  for (let i = 0; i < teams.length; i += 1) {
+    const team = teams[i];
+
+    if (team.id === Number(id)) {
+      team.name = name;
+      team.initials = initials;
+      updatedTeam = team;
+    }
+  }
+
+  res.status(200).json({ updatedTeam });
+});
+
+// Delete
+app.delete("/teams/:id", (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(200).end();
+});
 
 module.exports = app;
